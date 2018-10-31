879042f4afd8043886d3cef421bbe2bdc63efd57
Dung Van Nguyen <dungnv.itdn@gmail.com>
2018-10-31 09:40:24 +0100

    update

--- src/test/java/football/sharing/HelloFootballSharingApplicationTests.java
+++ src/test/java/football/sharing/HelloFootballSharingApplicationTests.java
@@ -17,8 +17,8 @@ public class HelloFootballSharingApplicationTests {
 
 	@Test
 	public void testHelloWorld() {
-		String check = "hello world";
-		assertEquals(check, "hello world");
+		String check = "hello word";
+		assertEquals(check, "hello word");
 	}
 
 }
