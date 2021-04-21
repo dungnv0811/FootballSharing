711f4f368fc2bc269602664311a3d12641d4d5d1
Dung Van Nguyen <Dung.Van.Nguyen@mgm-tp.com>
2021-04-21 10:54:56 +0700

    move to gradle

--- src/main/java/football/sharing/FootballSharingApplication.java
+++ src/main/java/football/sharing/FootballSharingApplication.java
@@ -3,7 +3,6 @@ package football.sharing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FootballSharingApplication {
 
